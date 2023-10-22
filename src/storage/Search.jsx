93833376bed7d6cda2036b3c769abc9046a11e10
Riju Mondal <pilotrijurocks@gmail.com>
2023-10-23 00:42:08 +0530

    Second Day Push

--- src/storage/Search.jsx
+++ src/storage/Search.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
-import { mockSearchResults } from "../../test/Mock"; 
+import { mockSearchResults } from "../../test/Mock";
+import { XMarkIcon, MagnifyingGlassIcon} from '@heroicons/react/24/solid';
+import SearchResults from "./SearchResult";
 
-
+ 
 const Search = () => {
   const [input, setInput] = useState("");
   const [bestMatches, setBestMatches] = useState(mockSearchResults.result);
@@ -16,7 +18,7 @@ const Search = () => {
   };
 
   return (
-    <div className="flex items-center my-4 border-2 rounded-md relative z-50 w-75 bg-black border-neutral-200">
+    <div className="flex items-center my-4 border-2 rounded-md relative z-50 w-75 bg-white border-neutral-200">
       <input
         type="text"
         value={input}
@@ -31,8 +33,22 @@ const Search = () => {
           }
         }}
       />
+
+      {input && <button onClick={clear}>
+        <XMarkIcon className="w-5 h-5 bg-white" />
+      </button>}
+       
+       <button className="w-8 h-8 bg-gray-600 rounded-md flex justify-center items-center mr-2">
+        <MagnifyingGlassIcon className="w-5 h-5 text-white" />
+      </button>
+
+      
+
+      {input && bestMatches.length > 0 ? <SearchResults results={bestMatches} /> : null}
+
     </div>
   );
 };
 
 export default Search;
+
