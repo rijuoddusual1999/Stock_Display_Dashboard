f2bd85e0dcfcf5d96b6eac968f52cb7cfa58d1e0
Riju Mondal <pilotrijurocks@gmail.com>
2023-10-26 05:48:32 +0530

    Chart Bugs

--- src/storage/Charts.jsx
+++ src/storage/Charts.jsx
@@ -1,6 +1,5 @@
-import React, { useState } from "react";
-import { mockHistoricalData } from "../../test/Mock.jsx";
-import { convertUnixTimestampToDate } from "../converter/Converter.jsx";
+import React, { useContext, useEffect, useState } from "react";
+import { convertUnixTimestampToDate, convertDateToUnixTimestamp, createDate} from "../converter/Converter.jsx";
 import {
   Area,
   XAxis,
@@ -12,14 +11,58 @@ import {
 import Container from "./Container";
 import {chartConfig} from "../../test/Test.jsx";
 import ChartFilter from "./ChartFilter.jsx";
-
+import { fetchHistoricalData } from "../API/stock-api.jsx";
+import StockContext from "../context/ContextStocks.jsx";
 
 
 const Chart = () => {
-  const [data, setData] = useState(mockHistoricalData);
+  const [data, setData] = useState([]);
   const [filter, setFilter] = useState("1W");
+  const {stockSymbol} = useContext(StockContext);
+
+
+  useEffect(() => {
+    const getDateRange = () =>{
+
+      const {days,fivedays,months,threemonths,sixmonths,oneyear,fiveyear} =chartConfig[filter];
+
+      const endDate = new Date();
+
+      const startDate = createDate(endDate, -days, -fivedays, -months, -threemonths , -sixmonths , -oneyear, -fiveyear );
+
+      const startTimestampUnix = convertDateToUnixTimestamp(startDate);
+      const endTimestampUnix = convertDateToUnixTimestamp(endDate);
+
+      return {startTimestampUnix, endTimestampUnix};
+
+
+    }
+ 
+    const updateChartData = async () => {
+      try {
+        const { startTimestampUnix, endTimestampUnix } = getDateRange();
+        const resolution = chartConfig[filter].resolution;
+        const result = await fetchHistoricalData(
+          stockSymbol,
+          resolution,
+          startTimestampUnix,
+          endTimestampUnix
+        );
+        setData(formatData(result));
+      } catch (error) {
+        setData([]);
+        console.log(error);
+      }
+    };
+
+    updateChartData();
+   
+
+
+  },[stockSymbol, filter])
 
   const formatData = (data) => {
+
     return data.c.map((item, index) => {
       return {
         value: item.toFixed(2),
@@ -44,7 +87,7 @@ const Chart = () => {
         ))}
       </ul>
       <ResponsiveContainer>
-        <AreaChart data={formatData(data)}>
+        <AreaChart data={data}>
           <defs>
             <linearGradient id="chartColor" x1="0" y1="0" x2="0" y2="1">
               <stop
